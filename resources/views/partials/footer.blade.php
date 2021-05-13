e5421eaffe911566e5062064150bc50e4a784f57
emanuele <emattina97@gmail.com>
2021-05-13 16:58:50 +0200

    Home completa

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,85 @@
 <footer>
-    <p>footer</p>
+    <section class="footer-top">
+        <div class="container">
+            <div class="row mar-15">
+                <div class="col-2 pad-15">
+                    <ul>
+                        <li class="footer-title">dc comics</li>
+                        <li>Characters</li>
+                        <li>Comics</li>
+                        <li>Movies</li>
+                        <li>Tv</li>
+                        <li>Games</li>
+                        <li>Videos</li>
+                        <li>News</li>
+                    </ul>
+                    <ul>
+                        <li class="footer-title">shop</li>
+                        <li>Shop DC</li>
+                        <li>Shop DC Collectibles</li>
+                    </ul>
+                </div>
+                <div class="col-2 pad-15">
+                    <ul>
+                        <li class="footer-title">dc</li>
+                        <li>Tearm Of Use</li>
+                        <li>Privacy policy (New)</li>
+                        <li>Ad Choises</li>
+                        <li>Advertising</li>
+                        <li>Jobs</li>
+                        <li>Subscriptions</li>
+                        <li>Talent Workshops</li>
+                        <li>CPSC Certificates</li>
+                        <li>Ratings</li>
+                        <li>Shop Help</li>
+                        <li>Contact Us</li>
+                    </ul>
+                </div>
+                <div class="col-2 pad-15">
+                    <ul>
+                        <li class="footer-title">sites</li>
+                        <li>Characters</li>
+                        <li>Comics</li>
+                        <li>Movies</li>
+                        <li>Tv</li>
+                        <li>Games</li>
+                        <li>Videos</li>
+                        <li>News</li>
+                    </ul>
+                </div>
+                <div class="col-6">
+                    <div class="dc-footer-logo">
+                        <img src="/img/dc-logo-bg.png" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="footer-copyright">
+                    <span>All Site Content TM and © 2020 DC Entertainment, unless otherwise <a>noted here</a>. All rights reserved.</span>
+                    <span><a>Cookies Settings</a></span>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="footer-bottom">
+        <div class="container">
+            <div class="bottom-container">
+                <div class="bottom-left">
+                    <button class="btn-footer">sign-up now!</button>
+                </div>
+    
+                <div class="bottom-right">
+                    <span>follow us</span>
+                    <ul>
+                        <li><img src="/img/footer-facebook.png" alt=""></li>
+                        <li><img src="/img/footer-twitter.png" alt=""></li>
+                        <li><img src="/img/footer-youtube.png" alt=""></li>
+                        <li><img src="/img/footer-pinterest.png" alt=""></li>
+                        <li><img src="/img/footer-periscope.png" alt=""></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
 </footer>
