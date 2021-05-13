3bce5c0776c45acc754ba55c6fb44fbc5855c9a2
emanuele <emattina97@gmail.com>
2021-05-13 18:46:02 +0200

    Sezione descrizione comic

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -16,11 +16,38 @@
                 </div>
             </div>
         </div>
+        <div class="comic-info-section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-8">
+                        <h1>{{$comic['title']}}</h1>
+                        <div class="green-nav">
+                            <div class="green-nav-left">
+                                <span>U.S. Price: <span class="price">{{$comic['price']}}</span></span>
+                                <span>AVAILABLE</span>
+                            </div>
+                            <div class="green-nav-right">
+                                <nobr><span>Check Availability</span></nobr>
+                            </div>
+                        </div>
+                        <div class="comic-description">
+                            <p>{{$comic['description']}}</p>
+                        </div>
+                    </div>
+
+                    <div class="col-4">
+                        <div class="advertising">
+                            <span>advertisement</span>
+                            <img src="/img/adv.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         
 
 
 
 
-        {{-- <h1>{{$comic['title']}}</h1> --}}
     </section>
 @endsection
