72dc19afdd9786c3414ab8d75f49e732ee512fb4
emanuele <emattina97@gmail.com>
2021-05-13 17:41:20 +0200

    Bozza grafica singolo fumetto

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -1,8 +1,26 @@
 @extends('layouts.app')
 
 @section('main')
-    <section>
-        {{var_dump($comic)}}
-        <h1>{{$comic['title']}}</h1>
+    <section class="section-single-comic">
+        <div class="cover-section">
+            <div class="container">
+                <div class="cover">
+                    <div class="type">
+                        <span>{{$comic['type']}}</span>
+                    </div>
+                    <div class="view-gallery">
+                        <span>view gallery</span>
+                    </div>
+                    <img src="{{$comic['thumb']}}" alt="">
+                    
+                </div>
+            </div>
+        </div>
+        
+
+
+
+
+        {{-- <h1>{{$comic['title']}}</h1> --}}
     </section>
 @endsection
